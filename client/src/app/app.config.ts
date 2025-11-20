62744115572dbc66f87b096d521f508c44425bc7
nelsoncresp <nelsoncp2k18@gmail.com>
2025-11-20 14:35:06 -0500

    Fix 404 hash

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,15 +1,19 @@
+// Ubicación: client/src/app/app.config.ts
+
 import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+// Importar withHashLocation para resolver el problema del servidor 404
+import { provideRouter, withHashLocation } from '@angular/router'; 
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    importProvidersFrom(HttpClientModule),
-    provideHttpClient(
-      withInterceptors([authInterceptor])
-    )
-  ]
+ providers: [
+  // CAMBIO CLAVE: Se añade withHashLocation()
+  provideRouter(routes, withHashLocation()), 
+  importProvidersFrom(HttpClientModule),
+  provideHttpClient(
+   withInterceptors([authInterceptor])
+  )
+ ]
 };
